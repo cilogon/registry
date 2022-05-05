3913ba8d1e428182bcf4a670726c8fe1bdbe470f
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2022-05-05 22:02:48 +0300

    Add People View index filtering

--- app/src/Model/Table/PeopleTable.php
+++ app/src/Model/Table/PeopleTable.php
@@ -44,6 +44,7 @@ class PeopleTable extends Table {
   use \App\Lib\Traits\QueryModificationTrait;
   use \App\Lib\Traits\TableMetaTrait;
   use \App\Lib\Traits\ValidationTrait;
+  use \App\Lib\Traits\SearchFilterTrait;
   
   /**
    * Perform Cake Model initialization.
