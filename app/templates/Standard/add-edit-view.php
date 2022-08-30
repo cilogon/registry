b736533c422490f1a8f5cf32b0aad53173c14432
Arlen Johnson <arlen@sphericalcowgroup.com>
2022-08-30 14:47:28 -0400

    Hide "ID:" label on entity add (CFM-202) (#47)

--- app/templates/Standard/add-edit-view.php
+++ app/templates/Standard/add-edit-view.php
@@ -134,7 +134,9 @@ if(!empty($hidden)) {
   }
 }
 
-print '<li id="cm-entity-id">' . __d('information', 'entity.id', $vv_obj->id) . '</li>'; 
+if($vv_action != 'add') {
+  print '<li id="cm-entity-id">' . __d('information', 'entity.id', $vv_obj->id) . '</li>';  
+}
 
 if($vv_action == 'add' || $vv_action == 'edit') {
   // We don't want/need to output these for view actions
