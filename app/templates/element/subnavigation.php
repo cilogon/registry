41157056295cbb225a2c331e4bf13980786f77c0
Arlen Johnson <arlen@sphericalcowgroup.com>
2023-06-29 16:49:55 -0400

    Quick fix to Properties tab to restore "edit" as the default action (CFM-299) (#108)

--- app/templates/element/subnavigation.php
+++ app/templates/element/subnavigation.php
@@ -207,7 +207,8 @@ if(!empty($vv_person_name)) {
             print $this->Html->link(
               __d('controller', 'People', [1]),
               [ 'controller' => 'people',
-                'action' => $curAction == 'edit' ? 'edit' : 'view',
+                // TODO: the following test needs to be made based on read-only status of the Person
+                'action' => $curAction == 'view' ? 'view' : 'edit',
                 $curId
               ],
               ['class' => $linkClass]
@@ -267,7 +268,8 @@ if(!empty($vv_person_name)) {
             print $this->Html->link(
               __d('controller', 'Properties', [99]),
               [ 'controller' => 'groups',
-                'action' => $curAction == 'edit' ? 'edit' : 'view',
+                // TODO: the following test needs to be made based on read-only status of the group
+                'action' => $curAction == 'view' ? 'view' : 'edit',
                 $curId
               ],
               ['class' => $linkClass]
