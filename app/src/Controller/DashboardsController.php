2c8a7d28c8cca9c65c381f7ad1b6d96f4072a524
Arlen Johnson <arlen@sphericalcowgroup.com>
2023-10-05 15:13:43 -0400

    Revert global search GET to POST (CFM-323) (#128)

--- app/src/Controller/DashboardsController.php
+++ app/src/Controller/DashboardsController.php
@@ -311,9 +311,9 @@ class DashboardsController extends StandardController {
 
     // Gather our search string.
     $q = '';
-    if(!empty($this->request->getQuery('q'))) {
+    if(!empty($this->request->getData('q'))) {
       // A search was passed in from the form on the Global Search bar. 
-      $q = trim($this->request->getQuery('q'));
+      $q = trim($this->request->getData('q'));
     }
   
     // Only process the request if we have a string of non-space characters
@@ -398,7 +398,7 @@ class DashboardsController extends StandardController {
       
       $this->Flash->information(__d('result',
                                     'search.exact',
-                                    [filter_var($this->request->getQuery('q'), FILTER_SANITIZE_SPECIAL_CHARS),
+                                    [filter_var($this->request->getData('q'), FILTER_SANITIZE_SPECIAL_CHARS),
                                      __d('controller', $matchClass, [1])]));
 
       // Redirect to the matchClass controller
